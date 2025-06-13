10b5a7ed5987439745b4c89f1eb91eddb1af4d11
Mykhailo Sheleh <shelegmike@gmail.com>
2025-06-13 14:27:55 +0300

    feat(prowler): adapt to pydantic v2 and latest pyocsf-models

--- prowler/lib/outputs/ocsf/ocsf.py
+++ prowler/lib/outputs/ocsf/ocsf.py
@@ -4,8 +4,8 @@ from typing import List
 
 from py_ocsf_models.events.base_event import SeverityID, StatusID
 from py_ocsf_models.events.findings.detection_finding import DetectionFinding
-from py_ocsf_models.events.findings.detection_finding import (
-    TypeID as DetectionFindingTypeID,
+from py_ocsf_models.events.findings.detection_finding_type_id import (
+    DetectionFindingTypeID,
 )
 from py_ocsf_models.events.findings.finding import ActivityID, FindingInformation
 from py_ocsf_models.objects.account import Account, TypeID
